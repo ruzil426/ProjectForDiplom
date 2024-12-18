a9b99c2791764a7072cf77294d6ac33423e7f79f
ruzil426 <ruzil426@mail.ru>
2024-12-18 14:52:20 +0300

    Создание

---
+++ ProjectDiplom/shop/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+from models import db, Shop
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db.init_app(app)
+
+@app.route('/', methods=['GET'])
+def index():
+    products = Shop.query.all()
+    return render_template('index.html', products=products)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        weight = request.form['weight']
+        quantity = request.form['quantity']
+        price = request.form['price']
+        new_item = Shop(name=name, description=description, weight=weight, quantity=quantity, price=price)
+        db.session.add(new_item)
+        db.session.commit()
+        return redirect('/')
+    return render_template('add.html')
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete_product(id):
+    product = Shop.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/promotion', methods=['GET'])
+def promotion():
+    return render_template('promotion.html')
+
+if __name__ == '__main__':
+    app.run()
